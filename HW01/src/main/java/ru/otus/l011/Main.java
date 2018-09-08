b5ca0d1b836ed14c3659a82ae5baa9d0f1e07c3a
Aeuge <ifti@yandex.ru>
2018-09-08 16:22:43 +0300

    woking obfuscation

--- HW01/src/main/java/ru/otus/l011/Main.java
+++ HW01/src/main/java/ru/otus/l011/Main.java
@@ -1,30 +1,11 @@
 package ru.otus.l011;
 
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 
 import java.util.*;
 
-/**
- * Created by tully.
- *
- * Example for L01.1
- *
- * To start the application:
- * mvn package
- * java -cp ./target/L01.1-maven.jar ru.otus.l011.Main
- * java -jar ./target/L01.1-maven.jar //java.lang.NoClassDefFoundError: com/google/common/collect/Lists
- * java -cp ./target/L01.1-maven.jar;C:\Users\Tully\.m2\repository\com\google\guava\guava\25.0-jre\guava-25.0-jre.jar ru.otus.l011.Main
- *
- * To unzip the jar:
- * 7z x -oJAR ./target/L01.1-maven.jar
- * unzip -d JAR ./target/L01.1-maven.jar
- *
- * To build:
- * mvn package
- * mvn clean compile
- * mvn assembly:single
- * mvn clean compile assembly:single
- */
+
 public class Main {
     private static final int MEASURE_COUNT = 1;
 
@@ -36,9 +17,7 @@ public class Main {
             example.add(i);
         }
 
-        List<Integer> result = new ArrayList<>();
-        Collections.shuffle((List<Integer>)example);
-        calcTime(() -> result.addAll(Lists.reverse((List<Integer>)example)));
+        calcTime(() -> Collections2.permutations((List<Integer>)example));
     }
 
     private static void calcTime(Runnable runnable) {
