99b46369335c7677b10cfb138d35f0a283a4f2d7
Aeuge <ifti@yandex.ru>
2018-09-16 15:08:10 +0300

    hw2

--- HW02/src/main/java/ru/otus/agent/A.java
+++ HW02/src/main/java/ru/otus/agent/A.java
@@ -1,7 +1,10 @@
 package ru.otus.agent;
 
 public class A {
+        int i1;
+        int i2;
         Integer id;
         String name;
         String name2="test";
+        public Integer id2=0;
 }
--- HW02/src/main/java/ru/otus/agent/MainC.java
+++ HW02/src/main/java/ru/otus/agent/MainC.java
@@ -1,5 +1,10 @@
 package ru.otus.agent;
 
+import com.google.common.primitives.Primitives;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.util.*;
 import static ru.otus.agent.MyAgent.getObjectSize;
@@ -7,20 +12,59 @@ import static ru.otus.agent.MyAgent.getObjectSize;
 //usage: java -javaagent:target/aristov02.jar -jar target/aristov02.jar
 public class MainC {
     public static void main(String[] args) {
-        printObjectSize(new Object());
-        printObjectSize(new A());
-        printObjectSize(1);
-        printObjectSize("string");
-        printObjectSize("огрооооомнаяяяяя строккааааааааааааа");
-        printObjectSize(Calendar.getInstance());
-        printObjectSize(new BigDecimal("999999999999999.999"));
+        int i = 1;
         printObjectSize(new ArrayList<String>());
+        printObjectSize("string");
+        printObjectSize(i);
         printObjectSize(new Integer[100]);
+        printObjectSize(new A());
+        printObjectSize(new int[100]);
     }
 
+    public static int SizeOf (Object obj) {
+        Class c = obj.getClass();
+        int size = 0;
+        if (obj instanceof Object[]) {
+            System.out.println("массив");
+            if (c.isPrimitive()) {
+                System.out.println("простой тип "+c);
+            } else {
+                System.out.println("SubObject: "+String.format("%s, %s, size=%s", obj, c, getObjectSize(c)));
+                size += (int) getObjectSize(c);
+                size += (int) SizeOf(c);
+            }
+            size = (int)getObjectSize(c.getClass())*((Object[]) obj).length;
+        } else {
+            Field fields[] = c.getFields();
+            System.out.println("всего полей: "+fields.length);
+            for (Field item:fields) {
+                if (item.getType().isPrimitive()) {
+                    System.out.println("простой тип "+item.getType());
+                } else {
+                    System.out.println("SubObject:"+String.format("%s, %s, size=%s", item, item.getType(), getObjectSize(item.getType())));
+                    size += (int) getObjectSize(item.getType());
+                    size += (int) SizeOf(item.getType());
+                }
+            }
+        }
+        return size;
+    }
+    
+    
     public static void printObjectSize(Object obj) {
-        System.out.println(String.format("%s, size=%s", obj.getClass()
-                .getSimpleName(), getObjectSize(obj)));
+        try {
+            Class c = obj.getClass();
+            int size = 0;
+            size = (int) getObjectSize(obj);
+            System.out.println("Object:"+String.format("%s, size=%s", c, size));
+            size += SizeOf (obj);
+            System.out.println("Итоговый размер объейта"+String.format(" %s, size=%s ",c, size));
+
+        }
+        catch(Exception e) {
+            System.out.println("Exception: " + e) ;
+        }
+
     }
 
 
