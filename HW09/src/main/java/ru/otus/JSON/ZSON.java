26effc65a9be33041a241d8bc0827cc223c4de24
aeuge <ifti@yandex.ru>
2018-11-19 12:21:15 +0300

    hw09 v2.01

--- HW09/src/main/java/ru/otus/JSON/ZSON.java
+++ HW09/src/main/java/ru/otus/JSON/ZSON.java
@@ -11,6 +11,30 @@ public class ZSON {
     private static Logger logger = LoggerFactory.getLogger(ZSON.class);
     transient static Object[] myData;
 
+    public static String convertClassToJSON(Object obj) throws ClassNotFoundException, IllegalAccessException {
+        if (obj!=null) {
+            Class<?> cls = obj.getClass();
+            Field[] fields = cls.getDeclaredFields();
+            String resultstr = "{";
+
+            for (Field f : fields) {
+                boolean isTransient = Modifier.isTransient(f.getModifiers());
+                if (!isTransient) {
+                    f.setAccessible(true);
+                    if (f.get(obj) != null) {
+                        if (resultstr.length() > 1) {
+                            resultstr += ",";
+                        }
+                        resultstr += "\"" + f.getName() + "\":" + convertObjectToJSON(f.get(obj));
+                    }
+                }
+            }
+            return resultstr + "}";
+        } else {
+            return "";
+        }
+    }
+
     private static String convertCollectionObjectToJSON(Object obj) throws ClassNotFoundException, IllegalAccessException {
         List<String> resultList = new ArrayList<>();
         Iterator iterator = ((Collection) obj).iterator();
@@ -36,11 +60,7 @@ public class ZSON {
     }
 
     private static String addQuotas(String s) {
-        String str = s;
-        if (str.charAt(0)!='\"') {
-            str = "\""+str+"\"";
-        }
-        return str;
+        return s.charAt(0)!='\"' ? "\""+s+"\"" : s;
     }
 
     private static String convertObjectToJSON(Object obj) throws ClassNotFoundException, IllegalAccessException {
@@ -78,47 +98,6 @@ public class ZSON {
         }
     }
 
-    private static String convertInnerObjectToJSON__(Object obj) throws ClassNotFoundException, IllegalAccessException {
-        String canonicalName = obj.getClass().getCanonicalName();
-        canonicalName = canonicalName.replace("java.lang.","");
-        switch (canonicalName) {
-            case "Integer":
-            case "Float":
-            case "Double":
-            case "Short":
-            case "Long":
-            case "Byte":
-            case "Boolean":  return obj.toString();
-            case "Character":
-            case "String":   return "\"" + obj.toString() + "\"";
-            default: return convertClassToJSON(obj);
-        }
-    }
-
-
-    public static String convertClassToJSON(Object obj) throws ClassNotFoundException, IllegalAccessException {
-        if (obj!=null) {
-            Class<?> cls = obj.getClass();
-            Field[] fields = cls.getDeclaredFields();
-            String resultstr = "{";
-
-            for (Field f : fields) {
-                boolean isTransient = Modifier.isTransient(f.getModifiers());
-                if (!isTransient) {
-                    f.setAccessible(true);
-                    if (f.get(obj) != null) {
-                        if (resultstr.length() > 1) {
-                            resultstr += ",";
-                        }
-                        resultstr += "\"" + f.getName() + "\":" + convertObjectToJSON(f.get(obj));
-                    }
-                }
-            }
-            return resultstr + "}";
-        } else {
-            return "";
-        }
-    }
 
     public static boolean isMap(Object ob) {
         return ob instanceof Map ;
