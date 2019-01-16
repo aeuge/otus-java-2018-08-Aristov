c745a2903ba8b0138b312560b36b074e430966dc
Aeuge <ifti@yandex.ru>
2019-01-16 23:22:42 +0300

    hw12 cosmetic commit

--- HW12/src/main/java/ru/otus/web/Main.java
+++ HW12/src/main/java/ru/otus/web/Main.java
@@ -2,26 +2,17 @@ package ru.otus.web;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import ru.otus.web.DBservice.DBService;
-import ru.otus.web.DBservice.DBServiceHibernateImpl;
-import ru.otus.web.dataset.AddressDataSet;
-import ru.otus.web.dataset.PhoneDataSet;
-import ru.otus.web.dataset.UsersDataSet;
-import ru.otus.web.db.DBDataInitializer;
 import ru.otus.web.servlet.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     private final static int PORT = 8090;
 
-
     public static void main(String[] args) throws Exception {
         Server server = new Server(PORT);
         server.setHandler(new HandlerList(ServletInitializer.initResourceHandler(), ServletInitializer.initContext()));
-
         server.start();
         server.join();
     }
